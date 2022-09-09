6bb0b9e7d1e15b8131b88e3102687cdc04b138de
OreosMagic <gtontoh@gmail.com>
2022-09-09 11:46:25 -0700

    9-print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9' ; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
 
 		if (i != '9')
 		{
 			putchar(',');
-			putchar(',');
+			putchar(' ');
 		}
 	}
 
