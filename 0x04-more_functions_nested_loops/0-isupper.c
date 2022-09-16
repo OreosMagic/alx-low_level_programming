3e151010b5b0ea7226fe28368a1f1b2394165120
OreosMagic <gtontoh@gmail.com>
2022-09-16 11:13:55 -0700

    hot fix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isupper - is a function that checksi if alphabet is uppercase
+ *
+ * @c: takes in parameter c and checks
+ *
+ * Return: Always return 0 or 1
+ */
+int _isupper(int c)
+{
+	int tmp;
+
+	if (c >= 65 && c <= 90)
+
+		tmp = 1;
+
+	else
+		tmp = 0;
+
+	return (tmp);
+}
