90b702600d6d4f9920601844bcb53f9c94cf7b38
OreosMagic <gtontoh@gmail.com>
2022-09-17 13:24:23 -0700

    hot fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,21 @@
  */
 int main(void)
 {
-	int a = 1, i = 2;
+	int i;
 
-	printf("%d", a);
-	while (i < 101)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf(" FizzBuzz");
-		else if (i % 3 == 0)
-			printf(" Fizz");
-		else if (i % 5 == 0)
-			printf(" Buzz");
+		if (i == 100)
+			printf("Buzz");
+		else if (((i % 3) == 0) && ((i % 5) ==0))
+			printf("FizzBuzz ");
+		else if ((i % 3) == 0)
+			printf("Fizz ");
+		else if ((i % 5) == 0)
+			printf("Buzz ");
 		else
 			printf(" %d", i);
-		i++;
 	}
-	printf("\n');
+	printf("\n");
 	return (0);
 }
