5caec087a39aef8cf566968a6c7d374932e3d864
OreosMagic <gtontoh@gmail.com>
2022-09-22 22:49:53 -0700

    hot fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * print_line - prints a s bytes of a buffer
  * @c: buffer to print
@@ -23,7 +23,7 @@ void print_line(char *c, int s, int l)
 	}
 	for (k = 0; k <= s; k++)
 	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+		if (c[l 10 + k] > 31 && c[l 10 + k] < 127)
 			putchar(c[l * 10 + k]);
 		else
 			putchar('.');
