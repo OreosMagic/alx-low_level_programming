06cd8966846ad8a96ce871554fa907db03c86030
OreosMagic <gtontoh@gmail.com>
2022-09-22 22:40:28 -0700

    hot fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,8 +1,7 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * print_line - prints a s byte of a buffer
+ * print_line - prints a s bytes of a buffer
  * @c: buffer to print
  * @s: bytes of buffer to print
  * @l: line of buffer to print
