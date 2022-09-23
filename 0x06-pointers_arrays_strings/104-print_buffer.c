3957fe50521d989d0bd3a96bc7d74016f9ab2718
OreosMagic <gtontoh@gmail.com>
2022-09-22 22:24:08 -0700

    hot fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -42,9 +42,9 @@ void print_buffer(char *b, int size)
 {
 	int i;
 
-	for (i = 0; i <= (size - 1) / 10 && size, i++)
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-		printf("%08x:", i * 10);
+		printf("%08x: ", i * 10);
 		if (i < size / 10)
 		{
 			print_line(b, 9, i);
