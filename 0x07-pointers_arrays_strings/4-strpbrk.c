f601d9d85fda00679defa91e6c7cec4425b89841
OreosMagic <gtontoh@gmail.com>
2022-09-26 21:06:03 -0700

    locates first occurence of any bytes from accept in s

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,22 +1,29 @@
 #include "main.h"
 
 /**
- * _strpbrk - locates first occurence of any bytes from accept in s
- * @s: string to work on
- * @accept: pattern to match against
- * Return: pointer to the first match
+ * _strpbrk -a function that searches a
+ * string for any of a set of bytes
+ * @s: pointer to input string
+ * @accept: pointer to string we
+ * searching for in @s
+ * Return: pointer to the bytes in @s
+ * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
 			if (s[i] == accept[j])
 				return (s + i);
+			j++;
 		}
+		i++;
 	}
-	return (NULL);
+	return ('\0');
 }
